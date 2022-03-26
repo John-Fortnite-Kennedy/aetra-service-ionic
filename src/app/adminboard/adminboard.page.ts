ae21dedefcb661719972ea323ac9b2591d86bb2d
akiayane <aitbek.ayaganov@gmail.com>
2022-03-26 23:32:39 +0600

    upd

--- src/app/adminboard/adminboard.page.ts
+++ src/app/adminboard/adminboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-adminboard',
@@ -7,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminboardPage implements OnInit {
 
-  constructor() { }
+  currentRequests = []
+
+  constructor() { 
+    this.connectToWebsocket()
+  }
+
+  connectToWebsocket(){
+    var ws = webSocket('ws://185.22.64.115:4004/api/ws');
+
+    ws.subscribe(
+      msg => {
+        var obj = JSON.parse(JSON.stringify(msg))
+        console.log(obj)
+        if(obj.payload){
+          this.currentRequests = obj.payload
+        }else if(obj.insert){
+          this.currentRequests.unshift(obj.insert)
+        }else if(obj.update){
+          this.currentRequests[obj.update.id] = obj.update
+        }
+      }, // Called whenever there is a message from the server.
+
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+
+      () => {
+        setInterval(this.connectToWebsocket, 5000)
+      } // Called when connection is closed (for whatever reason).
+    );
+  }
 
   ngOnInit() {
   }
