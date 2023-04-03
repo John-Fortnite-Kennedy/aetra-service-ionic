240f941d4e4fa6f26242753c1d90d8989f3f8301
alibek <topsnek495@gmail.com>
2023-04-04 02:27:01 +0600

    init

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +10,20 @@ import { ApiService } from '../api.service';
 })
 export class LoginPage implements OnInit {
 
-  login;
+  phone;
   password;
+  action = true;
+  segmentValue ="Логин";
 
-  constructor(public api: ApiService, public router: Router) { }
+  constructor(public api: ApiService, public router: Router, private animationCtrl: AnimationController) { }
 
   ngOnInit() {
     sessionStorage.removeItem('manager_access_data');
   }
 
-  tryAuth(login, password){
+  tryAuth(phone, password){
     var data = {
-      "login": login,
+      "login": phone,
       "password": password
     }
     console.log(data)
@@ -45,6 +48,65 @@ export class LoginPage implements OnInit {
       { type: 'minlength', message: 'Пароль не может быть короче 5 символов.' }
       // { type: 'pattern', message: 'Пароль должен содержать как минимум 1 заглавную букву, 1 строчную букву и 1 число.' }
     ],
+  }
+
+  async swap(){
+    if(this.action){
+      this.slideR()
+    } else {
+      this.slideL()
+    }
+    await this.delay(500);
+    this.action=!this.action;
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  segmentChanged(e){
+    console.log(e.detail.value)
+    this.segmentValue = e.detail.value;
+  }
+
+  slideR() {
+    let sign = document.getElementById('sign').getBoundingClientRect().width;
+    let reg = document.getElementById('reg').getBoundingClientRect().width;
+    let wrap = document.getElementById('wrap').getBoundingClientRect().width;
+    //console.log(myElement.getBoundingClientRect().width)
+    let slideRight = this.animationCtrl
+      .create()
+      .addElement(document.querySelector('.signIn'))
+      .duration(1000)
+      .fromTo('transform', 'translateX(0%)', 'translateX(calc(0% + '+(reg)+'px))')
+    slideRight.play()
+
+    let slideleft = this.animationCtrl
+      .create()
+      .addElement(document.querySelector('.signUp'))
+      .duration(1000)
+      .fromTo('transform', 'translateX(0%)', 'translateX(calc(100% - '+(wrap)+'px))')
+      slideleft.play()
+  }
+
+  slideL() {
+    let sign = document.getElementById('sign').getBoundingClientRect().width;
+    let reg = document.getElementById('reg').getBoundingClientRect().width;
+    let wrap = document.getElementById('wrap').getBoundingClientRect().width;
+    //console.log(myElement.getBoundingClientRect().width)
+    let slideRight = this.animationCtrl
+      .create()
+      .addElement(document.querySelector('.signIn'))
+      .duration(1000)
+      .fromTo('transform', 'translateX(calc(0% + '+(reg)+'px))', 'translateX(0%)')
+    slideRight.play()
+
+    let slideleft = this.animationCtrl
+      .create()
+      .addElement(document.querySelector('.signUp'))
+      .duration(1000)
+      .fromTo('transform', 'translateX(calc(100% - '+(wrap)+'px))', 'translateX(0%)')
+      slideleft.play()
   }
 
 }
